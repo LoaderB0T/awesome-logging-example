398d932f1346763f4951e9e69d1927b063c566bc
Janik Schumacher <janik.schumacher@pmcs.de>
2021-07-06 16:32:00 +0200

    .

---
+++ src/test.ts
@@ -0,0 +1,59 @@
+import chalk from 'chalk';
+import { AwesomeLogger } from 'awesome-logging';
+
+AwesomeLogger.log('');
+AwesomeLogger.log('');
+AwesomeLogger.log('');
+AwesomeLogger.log('');
+AwesomeLogger.log('');
+AwesomeLogger.log('');
+
+
+setInterval(() => {
+  AwesomeLogger.interrupt('interrupt' + Math.random());
+}, 4000);
+
+const promptForLog = () => {
+  const a = AwesomeLogger.prompt('choice', {
+    options: ['text', 'spinner', 'progressbar', 'placeholder1', 'placeholder2', 'placeholder3', 'placeholder4', 'placeholder5']
+  });
+
+  a.result.then(x => {
+    const logger = createExampleLogger(x)!;
+    AwesomeLogger.log(logger);
+    setTimeout(() => {
+      promptForLog();
+    }, 2000);
+  });
+};
+
+promptForLog();
+
+const createExampleLogger = (type: string) => {
+  switch (type) {
+    case 'text': {
+      return AwesomeLogger.create('text', { text: chalk.red('Example text') });
+    }
+    case 'spinner': {
+      return AwesomeLogger.create('spinner', {
+        text: ' My text',
+        spinnerDelay: 75,
+        spinnerFrames: [
+          chalk.magenta('■'),
+          chalk.magenta('▄'),
+          chalk.magenta('■'),
+          chalk.magenta('▀'),
+          chalk.magenta('▀'),
+          chalk.yellow('■'),
+          chalk.yellow('▄'),
+          chalk.yellow('■'),
+          chalk.yellow('▀'),
+          chalk.yellow('▀')
+        ]
+      });
+    }
+    case 'progressbar': {
+      return AwesomeLogger.create('progress', { totalProgress: 100, filledColor: 'GREEN', maxWidth: 100 });
+    }
+  }
+};
